577c88ab6aa3bd0308220c78d4bf95119be0b532
Camilo Balaguera <ca.balaguera@uniandes.edu.co>
2018-03-20 16:58:10 -0500

    Se creó la funcion get_fit, ademas se añadieron los archivos de texto al repositorio

--- CamiloBalaguera_analysis.py
+++ CamiloBalaguera_analysis.py
@@ -11,10 +11,10 @@ def normal_dist(x, mean, sigma):
 
 def get_fit(filename):
 	a = np.genfromtxt(filename, usecols=0)
-	np.histogram(a)
-	plt.hist(a, bins='auto')
-	plt.show()
-	popt, pcov = curve_fit(normal_dist, xdata, ydata)
+	histo = np.histogram(a, normed = true) #Hago un histograma que me retorna las frecuencias y los bins
+	plt.hist(a)
+	popt, pcov = curve_fit(normal_dist, 0.5*(histo[1][:-1]+histo[1][1:]), histo[0])
+	print(filename + str(popt[0]) + str(pcov[0]))
 
 	
 	
